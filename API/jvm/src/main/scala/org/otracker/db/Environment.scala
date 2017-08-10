c19d37215831303f67dfb3b714110b3a7eb6e0b5
tomas mccandless <tomas.mccandless@gmail.com>
2017-08-09 23:11:22 -0700

    use env vars for config overrides

--- API/jvm/src/main/scala/org/otracker/db/Environment.scala
+++ API/jvm/src/main/scala/org/otracker/db/Environment.scala
@@ -2,6 +2,8 @@ package org.otracker.db
 
 import com.typesafe.config.{Config, ConfigFactory}
 
+import scala.collection.JavaConverters.mapAsJavaMap
+
 /**
   * A utility object for reading configuration values.
   *
@@ -10,19 +12,27 @@ import com.typesafe.config.{Config, ConfigFactory}
   */
 object Environment {
 
+  private val driver: String = "com.mysql.jdbc.Driver"
+
   // TODO support h2 for unit tests
   private val defaults: String =
-    """
+    s"""
       |tracker {
-      |  driver = "com.mysql.jdbc.Driver"
-      |  url = "jdbc:mysql://localhost:3306/tracker"
-      |  user = "root"
-      |  password = "1234"
+      |  driver = $driver
+      |  url = "jdbc:mysql://localhost:3306/tracker?user=root&password=1234"
       |}
     """.stripMargin
 
-  // TODO double check if we can override these via jvm system properties
+  // for prod, we need to encode the real credentials as env vars
+  private val maybeUrl: Option[String] = Option(System.getenv("DB_URL"))
+
+  private val creds: Map[String, String] = maybeUrl match {
+    case Some(url) => Map("tracker.url" -> url)
+    case None => Map.empty
+  }
+
   val config: Config = ConfigFactory.systemEnvironment()
     .withFallback(ConfigFactory.systemProperties())
+    .withFallback(ConfigFactory.parseMap(mapAsJavaMap(creds)))
     .withFallback(ConfigFactory.parseString(defaults))
 }

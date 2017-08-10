32d941b7bc2f692015cba7f5b41e6ea1f156a956
tomas mccandless <tomas.mccandless@gmail.com>
2017-08-09 23:31:39 -0700

    parse agent id from request mapped body

--- API/jvm/src/main/scala/org/otracker/lambda/OffersForAgentHandler.scala
+++ API/jvm/src/main/scala/org/otracker/lambda/OffersForAgentHandler.scala
@@ -3,7 +3,7 @@ package org.otracker.lambda
 import com.amazonaws.services.lambda.runtime.Context
 import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
-import org.json4s.JValue
+import org.json4s.{DefaultFormats, JValue}
 import org.otracker.db.Db
 import org.otracker.db.Tables.OffersRow
 import org.pmw.tinylog.Logger
@@ -15,12 +15,17 @@ import org.pmw.tinylog.Logger
   */
 object OffersForAgentHandler extends HandlerLike {
 
+  implicit val formats = DefaultFormats
+
   /**
     *
     * @param request
     * @return response json
     */
   def computeResponse(request: JValue, context: Context): String = {
+
+    val agentId: Int = (request \ "params" \ "path" \ "agent_id").extract[String].toInt
+    Logger.info(s"agentId = $agentId")
 
     // TODO we need to get the id out of the path parameter
     val offers: Iterable[OffersRow] = Db.getOffersForAgent(1)

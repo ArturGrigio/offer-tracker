5422d40b163fe18ec2107b46c066c267d0d7eaf3
Todd LaMothe <toddlamothe@50-204-120-234-static.hfc.comcastbusiness.net>
2017-08-10 09:23:15 -0700

    Filter results based on specified agent ID

--- API/offers-search/index.js
+++ API/offers-search/index.js
@@ -2,8 +2,11 @@
 
 var AWS = require("aws-sdk");
 var mysql = require('mysql');
+var filters = "";
 
 exports.handler = (event, context, callback) => {
+    filters = event;
+    console.log("filters = ", filters);
     console.log("Querying database...");
     var result = queryDatabase(function(result) {
         console.log("about to return result 2:");
@@ -20,22 +23,10 @@ function queryDatabase(callback) {
           database : 'tracker'
     });
 
-    connection.connect(function(err){
+    connection.connect(function(err) {
           if(!err) {
-                console.log("Database is connected");
-                connection.query(`
-                  select
-                  	offers.address,
-                      offers .mls_number,
-                      agents.first_name "agent_first_name",
-                      agents.last_name "agent_last_name",
-                      agents.brokerage "agent_brokerage",
-                      agents.designations "agent_designations"
-                  from
-                  	offers
-                      join agent_offer on offers.id = agent_offer.offer_id
-                      join agents on agent_offer.agent_id = agents.id
-                  `, function (err, result, fields) {
+                console.log("database connected...");
+                connection.query(buildQuery(), function (err, result, fields) {
                     connection.end();
                     if (err) {
                         console.log("An error occured:");
@@ -52,4 +43,38 @@ function queryDatabase(callback) {
           }
     });
 
+    function buildQuery() {
+        var sql = `
+          select
+            offers.address,
+              offers .mls_number,
+              agents.id "agent_id",
+              agents.first_name "agent_first_name",
+              agents.last_name "agent_last_name",
+              agents.brokerage "agent_brokerage",
+              agents.designations "agent_designations"
+          from
+            offers
+              join agent_offer on offers.id = agent_offer.offer_id
+              join agents on agent_offer.agent_id = agents.id
+          ` + buildWhereClause();
+        console.log("sql: ");
+        console.log(sql);
+        return sql;
+    }
+
+    function buildWhereClause() {
+      var wc = "";
+      console.log("[buildWhereClause");
+      console.log("filters: ", filters);
+      if (filters.buyer_id || filters.seller_id || filters.agent_id) {
+        wc = " WHERE true "
+        //if (filters.buyer_id) wc += " AND ";
+        //if (filters.buyer_id) wc += " AND "
+        if (filters.agent_id) wc += " AND agents.id = " + filters.agent_id;
+        console.log("Where clause: ");
+        console.log(wc);
+      };
+      return wc;
+    };
 };

8ca596b2a5b9546bfba80a2f87e4cc2cdfe95e7c
Todd LaMothe <toddlamothe@MacBook-Pro-2.local>
2017-08-10 07:11:47 -0700

    Working offers-search function

--- API/offers-search/index.js
+++ API/offers-search/index.js
@@ -3,72 +3,53 @@
 var AWS = require("aws-sdk");
 var mysql = require('mysql');
 
-const mock_payload = [
-  {
-    "address": "123 Main Street",
-    "mls_number": "55454454",
-    "agent_first_name": "Jane",
-    "agent_last_name": "Doe",
-    "agent_brokerage": "XYZ Brokerage",
-    "agent_designations": "CPRS, e-Pro",
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-  },
-  {
-    "address": "456 Elm Street",
-    "mls_number": "098767890",
-    "agent_first_name": "Jane",
-    "agent_last_name": "Doe",
-    "agent_brokerage": "XYZ Brokerage",
-    "agent_designations": "CPRS, e-Pro",
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-  },
-  {
-    "address": "122 Andrews Ave",
-    "mls_number": "2465422332",
-    "agent_first_name": "Jane",
-    "agent_last_name": "Doe",
-    "agent_brokerage": "XYZ Brokerage",
-    "agent_designations": "CPRS, e-Pro",
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-  }
-]
-
-
-/*
-    Return offer search results that meet the specified criteria
-*/
 exports.handler = (event, context, callback) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
-    queryDatabase();
-    callback(null, mock_payload);
-
+    console.log("Querying database...");
+    var result = queryDatabase(function(result) {
+        console.log("about to return result 2:");
+        console.log(result);
+        callback(null, result);
+    });
 };
 
-function queryDatabase() {
+function queryDatabase(callback) {
     var connection = mysql.createConnection({
-          host     : 'chqgb39str0o.us-west-1.rds.amazonaws.com:3306',
-          user     : '<myusername>',
-          password : '<mypw>',
+          host     : 'tracker.chqgb39str0o.us-west-1.rds.amazonaws.com',
+          user     : 'tracker',
+          password : 't824>8vRfG)HKn',
           database : 'tracker'
     });
+
     connection.connect(function(err){
           if(!err) {
-                console.log("Database is connected ... nn");
+                console.log("Database is connected");
+                connection.query(`
+                  select
+                  	offers.address,
+                      offers .mls_number,
+                      agents.first_name "agent_first_name",
+                      agents.last_name "agent_last_name",
+                      agents.brokerage "agent_brokerage",
+                      agents.designations "agent_designations"
+                  from
+                  	offers
+                      join agent_offer on offers.id = agent_offer.offer_id
+                      join agents on agent_offer.agent_id = agents.id
+                  `, function (err, result, fields) {
+                    connection.end();
+                    if (err) {
+                        console.log("An error occured:");
+                        console.log(err);
+                    };
+                    console.log("about to return result 1:");
+                    console.log(result);
+                    callback(result);
+                  })
           }
           else {
                 console.log("Error connecting database ... nn");
+                console.log(err);
           }
     });
 
-    connection.query("INSERT INTO Users (user_id) VALUES ('TESTNAME')");
-    connection.end();
 };
